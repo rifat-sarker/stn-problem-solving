44a4aed2e5b0af2baa59a9f2f6aeb21e43743daa
rifat-sarker <rifatswd@gmail.com>
2025-06-19 18:44:54 +0600

    Add Task10.js to implement a function that calculates the total price from an array of objects using reduce

--- Task10.js
+++ Task10.js
@@ -1,2 +1,15 @@
 // 10. Task: Functional Programming - Reduce--
 // -Write a function that uses the reduce function to calculate the total value of an array of objects with a 'quantity' and 'price' property.interview
+
+const items = [
+  { quantity: 2, price: 10 },
+  { quantity: 1, price: 20 },
+  { quantity: 3, price: 5 },
+];
+
+function calculateTotalPrice(items) {
+  return items.reduce((total, item) => total + item.quantity * item.price, 0);
+}
+
+const totalPrice = calculateTotalPrice(items);
+console.log("Total Price:", totalPrice);
