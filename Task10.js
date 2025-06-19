7d86c474f3a059e0873084d45a411ad03d33f908
rifat-sarker <rifatswd@gmail.com>
2025-06-19 18:38:46 +0600

    Add Task10.js for functional programming task and update Task9.js with student sorting implementation

---
+++ Task10.js
@@ -0,0 +1,2 @@
+// 10. Task: Functional Programming - Reduce--
+// -Write a function that uses the reduce function to calculate the total value of an array of objects with a 'quantity' and 'price' property.interview
--- Task9.js
+++ Task9.js
@@ -1,16 +1,33 @@
-
-
-
 // 09.Task: Advanced Sorting
-
-
-
 // -Create an array of objects representing students with 'name' and 'grades' properties. Write a function to sort the students by average grade in descending order.
 
+const students = [
+  {
+    name: "Rifat",
+    grades: [85, 90, 78],
+  },
+  {
+    name: "Fatema",
+    grades: [92, 88, 95],
+  },
+  {
+    name: "Tania",
+    grades: [70, 75, 80],
+  },
+  {
+    name: "Jony",
+    grades: [88, 85, 90],
+  },
+];
 
-
-// 10. Task: Functional Programming - Reduce
-
-
-
-// -Write a function that uses the reduce function to calculate the total value of an array of objects with a 'quantity' and 'price' property.interview
+function sortStudentsByAverageGrade(students) {
+  return students.sort((a, b) => {
+    const averageA =
+      a.grades.reduce((sum, grade) => sum + grade, 0) / a.grades.length;
+    const averageB =
+      b.grades.reduce((sum, grade) => sum + grade, 0) / b.grades.length;
+    return averageB - averageA; 
+  });
+}
+const result = sortStudentsByAverageGrade(students);
+console.log(result);
