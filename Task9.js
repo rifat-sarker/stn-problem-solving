754f351925a7e30bad34f930422b546a576999d0
Rifat Sarker <103870701+rifat-sarker@users.noreply.github.com>
2025-06-20 17:57:42 +0600

    Update Task9.js

--- Task9.js
+++ Task9.js
@@ -1,5 +1,4 @@
-// 09.Task: Advanced Sorting
-// -Create an array of objects representing students with 'name' and 'grades' properties. Write a function to sort the students by average grade in descending order.
+// 09.Task: Advanced Sorting - Create an array of objects representing students with 'name' and 'grades' properties. Write a function to sort the students by average grade in descending order.
 
 const students = [
   {
